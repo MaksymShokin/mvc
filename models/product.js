56dcfeadc0bb995c0c8ac172082ecd5a1b0c8b49
Maksym Shokin <maksym.sh@upplabs.com>
2020-05-18 23:17:14 +0300

    refactoting

--- models/product.js
+++ models/product.js
@@ -1,45 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
+const filePath = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+)
+
+const getProductsFromFile = cb => {
+  fs.readFile(filePath, (error, fileContent) => {
+    if (error) {
+      return cb([]);
+    }
+      
+    cb(JSON.parse(fileContent));
+  })
+}
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const filePath = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    )
-
-    fs.readFile(filePath, (error, fileContent) => {
-      let products = [];
-      
-      if (!error) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(filePath, JSON.stringify(products), error => {
         console.log(error)
       });
-    })
+    });
   }
 
   static fetchAll(cb) {
-    const filePath = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    )
-
-    fs.readFile(filePath, (error, fileContent) => {
-      if (error) {
-        cb([]);
-      }
-        
-      cb(JSON.parse(fileContent));
-    })
+    getProductsFromFile(cb);
   }
 }
